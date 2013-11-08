2825af8ff68290d5b4ecb0996093b2487a23b7ca
cmichi <mich@elmueller.net>
2013-11-08 15:07:36 +0100

    minor hacking on sampleplugin

--- advanced-sampleplugin/advanced-sampleplugin/__init__.py
+++ advanced-sampleplugin/advanced-sampleplugin/__init__.py
@@ -6,6 +6,7 @@ import os
 _log = logging.getLogger(__name__)
 
 PLUGIN_DIR = os.path.dirname(__file__)
+foo1 = "haha"
 
 def setup_plugin():
     config = pluginapi.get_config('advanced-sampleplugin')
@@ -13,13 +14,21 @@ def setup_plugin():
         _log.info('%r' % config)
     else:
         _log.info('There is no configuration set.')
+    
+    foo2 = "huhu"
 
     pluginapi.register_template_path(os.path.join(PLUGIN_DIR, 'templates'))
 
     pluginapi.register_template_hooks(
-        {"persona_end": "advanced-sampleplugin/template.html"})
+        {"persona_end": "advanced-sampleplugin/template.html",
+        "media_foo": "advanced-sampleplugin/flattr.html"})
 
+def add_to_global_context(context):
+    context['foo'] = "tadaaa"
+    return context
+		     
 
 hooks = {
-    'setup': setup_plugin
+    'setup': setup_plugin,
+    'template_global_context': add_to_global_context
     }
