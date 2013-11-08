86778457133f7428fd80a71dd5814750815ef404
cmichi <mich@elmueller.net>
2013-11-08 16:34:58 +0100

    fixed advanced-sampleplugin

--- advanced-sampleplugin/advanced-sampleplugin/__init__.py
+++ advanced-sampleplugin/advanced-sampleplugin/__init__.py
@@ -6,25 +6,23 @@ import os
 _log = logging.getLogger(__name__)
 
 PLUGIN_DIR = os.path.dirname(__file__)
-foo1 = "haha"
 
 def setup_plugin():
+    global config 
     config = pluginapi.get_config('advanced-sampleplugin')
     if config:
         _log.info('%r' % config)
     else:
         _log.info('There is no configuration set.')
     
-    foo2 = "huhu"
-
     pluginapi.register_template_path(os.path.join(PLUGIN_DIR, 'templates'))
 
     pluginapi.register_template_hooks(
-        {"persona_end": "advanced-sampleplugin/template.html",
-        "media_foo": "advanced-sampleplugin/flattr.html"})
+        {"persona_end": "mediagoblin/advanced-sampleplugin/template.html"})
 
 def add_to_global_context(context):
-    context['foo'] = "tadaaa"
+    context['advancedsampleplugin'] = config
+    context['foo'] = 'bar'
     return context
 		     
 hooks = {
